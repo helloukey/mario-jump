20b2a8957b37489c014e1f12b18fe8f8501597eb
helloukey <kunalukey32@gmail.com>
2022-09-23 20:48:28 +0530

    PressAnyKey.css: reduced font-size for smaller screens. Obstacles.css: reduced obstacles sizes for smaller screens.

--- src/components/PressAnyKey.js
+++ src/components/PressAnyKey.js
@@ -2,7 +2,7 @@ import "./PressAnyKey.css";
 
 const PressAnyKey = () => {
   return (
-    <div>
+    <div className="press-container">
         <p className="press-title">ENTER KEY - START GAME</p>
         <p className="press-subtitle">SPACE KEY - JUMP!</p>
     </div>
