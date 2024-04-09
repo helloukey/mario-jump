f62945fff6f456a9e9bc069893cbb38c6761463d
Kunal <kunalukey32@gmail.com>
2024-04-10 00:05:06 +0530

    added e2e test for loading screen

---
+++ cypress/e2e/home/loading.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+describe("Check contents of the landing page", () => {
+  // Visit the landing page
+  beforeEach(() => {
+    cy.visit("/");
+  })
+
+  // Check for the mario image
+  it("Check for the mario image", () => {
+    cy.getCypress("loading-mario").should("exist");
+    cy.getCypress("loading-mario").should("be.visible");
+  });
+
+    // Check for the loading text
+    it("Check for the loading text", () => {
+      cy.getCypress("loading-loading-text").should("exist");
+      cy.getCypress("loading-loading-text").should("be.visible");
+      cy.getCypress("loading-loading-text").should("contain.text", "Loading...");
+    });
+
+    // Check for the enter button
+    it("Check for the enter button", () => {
+      cy.wait(6000);
+      cy.getCypress("loading-enter-button").should("exist");
+      cy.getCypress("loading-enter-button").should("be.visible");
+      cy.getCypress("loading-enter-button").should("contain.text", "ENTER");
+    });
+
+    // Check for the footer div
+    it("Check for the footer div", () => {
+      cy.getCypress("footer-copyright-div").should("exist");
+      cy.getCypress("footer-copyright-div").should("be.visible");
+      cy.getCypress("footer-copyright-div").should("contain.text", "Copyright ©");
+    });
+
+    // Check for the footer link
+    it("Check for the footer link", () => {
+      cy.getCypress("footer-copyright-link").should("exist");
+      cy.getCypress("footer-copyright-link").should("be.visible");
+      cy.getCypress("footer-copyright-link").should("contain.text", "Kunal Ukey");
+      cy.getCypress("footer-copyright-link").should("have.attr", "href", "https://github.com/helloukey");
+    });
+})
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="copyright">Copyright © {new Date().getFullYear()} {" "}
-     <a href="https://github.com/helloukey" target="_blank" rel="noreferrer" className="copyright-link">Kunal Ukey</a>
+    <div className="copyright" data-cy="footer-copyright-div">Copyright © {new Date().getFullYear()} {" "}
+     <a href="https://github.com/helloukey" target="_blank" rel="noreferrer" className="copyright-link" data-cy="footer-copyright-link">Kunal Ukey</a>
     </div>
   )
 }
