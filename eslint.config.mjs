05d17dfa8c4b15a712954993daf9a247fc0caa91
Kunal <kunalukey32@gmail.com>
2024-04-11 17:28:02 +0530

    updated eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,54 @@
 import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
+// mimic CommonJS variables -- not needed if using CommonJS
+import { FlatCompat } from "@eslint/eslintrc";
 import path from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
 });
 
-/* eslint import/no-anonymous-default-export: [2, {"allowArray": true}] */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
   { languageOptions: { globals: globals.browser } },
-  ...compat.extends("airbnb"),
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  ...compat.extends("prettier"),
   {
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
     },
-    extends: ["prettier"],
+  },
+  {
+    ignores: [
+      "cypress",
+      "node_modules",
+      "public",
+      "src/assets",
+      ".gitignore",
+      ".prettierrc",
+      ".prettierignore",
+      "cypress.config.js",
+      "eslint.config.mjs",
+    ],
   },
   eslintConfigPrettier,
 ];
